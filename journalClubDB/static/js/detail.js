4d23067e7677c85dba39c8db19ca421a59683cd5
michael <michael.chiang.mc5@gmail.com>
2015-11-11 02:06:47 -0800

    Fixed problem where selecting post history caused page to scroll all the way to the top
    Fixed display problem with reply/edit post buttons

--- journalClubDB/static/js/detail.js
+++ journalClubDB/static/js/detail.js
@@ -16,29 +16,7 @@ $(document).ready(function() {
   });
 });
 
-// view post history using dropdown
-$(document).ready(function() {
-  // only show last post/edit and hide all previous
-  $( ".comment-all").hide()
-  $( ".comment-last").show()
 
-  // dropdown to select which revision to view
-  $( "a[id*='action-']" ).click(function(e) {
-    var action_pk_counter = $(this).attr("id")
-    var arr = action_pk_counter.split('-')
-    var pk = arr[1]
-    var counter = arr[2]
-    $( ".li-dropdown-" + pk).removeClass("active");
-    $( ".li-dropdown-"+pk+"-"+counter).addClass("active");
-    var time_text = $( ".li-dropdown-"+pk+"-"+counter+ " > a" ).html();
-    $( "#comment-time-"+pk).html(time_text)
-    $( ".comment-target-all-" + pk).hide()
-    $( "#comment-text-" + pk + "-" + counter).show()
-  });
-
-
-
-});
 
 
 
