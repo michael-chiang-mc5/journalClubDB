90cc5d779a436a458c292897e186791a61ee51b3
Michael Chiang <michaelchiang@Michaels-iMac.local>
2015-12-01 16:23:56 -0800

    social auth redirect now handles switching between thread tabs in detail.html correctly

--- journalClubDB/static/js/detail.js
+++ journalClubDB/static/js/detail.js
@@ -6,7 +6,7 @@ function ChangeUrl(title, url) {
         var obj = { Title: title, Url: url };
         history.pushState(obj, obj.Title, obj.Url);
     } else {
-        alert("Please use a browser that supports HTML5 (like Google Chrome)");
+        alert("Please use a modern browser that supports HTML5 (like Google Chrome)");
     }
 }
 
@@ -23,11 +23,41 @@ $(document).ready(function() {
   }
 
   // Change url when different tabs
-  $( "li[id*='pill_']" ).click(function(event){
-    var pill_id = $(this).attr("id")
-    var arr = pill_id.split('_')
-    var id = arr[1]
-    ChangeUrl("","../"+id+"/")
+//  $( "li[id='pill']" ).click(function(event){
+  $( ".thread-switch-button" ).click(function(event){
+
+    //var pill_id = $(this).attr("id")
+    //var arr = pill_id.split('_')
+    //var id = arr[1]
+
+    // this changes url to reflect new thread without reloading
+    var threadnumber = $(this).attr("data-threadnumber")
+    ChangeUrl("","../"+threadnumber+"/")
+
+    // this changes social authentication redirect to go to new thread
+    $('.social-login-link').each(function(index) {
+
+      var link = $(this)
+      var original_href = link.attr('href')
+      var arr = original_href.split('/');
+
+      var reconstructed_str = ''
+      $.each(arr, function( index, value ) {
+        // this is the equivalent of continue if value==''
+        if (value == '') {
+          return
+        }
+        // this is the equivalent of break if index exceeds limit
+        if (index > arr.length-3 ) {
+          return false
+        }
+        reconstructed_str += '/' + value
+      });
+      reconstructed_str += '/' + threadnumber + '/'
+      link.attr('href',reconstructed_str)
+    });
+
+
   });
 
 
